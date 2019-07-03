7055617f4cdf34a369b838df73f886235f910953
池俊萍 <junping.chi@luckincoffee.com>
2019-07-03 18:03:50 +0800

    项目初始化

---
+++ src/main/java/com/com/dwStudy/lineage.scala
@@ -0,0 +1,12 @@
+package com.com.dwStudy
+
+/**
+  * @Author: junping.chi@luckincoffee.com
+  * @Date: 2019/7/3 18:01
+  * @Description:
+  */
+object lineage {
+  def main(args: Array[String]): Unit = {
+    println("hello world! ")
+  }
+}
