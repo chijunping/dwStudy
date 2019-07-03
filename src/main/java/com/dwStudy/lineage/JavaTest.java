00210aacbf428c18381b7a492301d3f12281b114
池俊萍 <junping.chi@luckincoffee.com>
2019-07-03 18:22:22 +0800

    项目初始化

---
+++ src/main/java/com/dwStudy/lineage/JavaTest.java
@@ -0,0 +1,12 @@
+package com.dwStudy.lineage;
+
+/**
+ * @Author: junping.chi@luckincoffee.com
+ * @Date: 2019/7/3 18:21
+ * @Description:
+ */
+public class JavaTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
