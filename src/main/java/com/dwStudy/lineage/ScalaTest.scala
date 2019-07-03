8e07e2986168ae0b0835c04271e75b404eef78de
池俊萍 <junping.chi@luckincoffee.com>
2019-07-03 18:14:58 +0800

    项目初始化

---
+++ src/main/java/com/dwStudy/lineage/ScalaTest.scala
@@ -0,0 +1,12 @@
+package com.dwStudy.lineage
+
+/**
+  * @Author: junping.chi@luckincoffee.com
+  * @Date: 2019/7/3 18:01
+  * @Description:
+  */
+object ScalaTest {
+  def main(args: Array[String]): Unit = {
+    println("hello world! ")
+  }
+}
--- src/test/java/com/dwStudy/lineage/ScalaTest02.scala
+++ src/test/java/com/dwStudy/lineage/ScalaTest02.scala
@@ -1,12 +1,13 @@
-package com.com.dwStudy
+package com.dwStudy.lineage
 
 /**
   * @Author: junping.chi@luckincoffee.com
-  * @Date: 2019/7/3 18:01
+  * @Date: 2019/7/3 18:10
   * @Description:
   */
-object lineage {
+object ScalaTest02 {
   def main(args: Array[String]): Unit = {
-    println("hello world! ")
+    println("Yes!")
   }
+
 }
